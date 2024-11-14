0028118e54bf36090037352a5bfa036ec68c7ed8
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-14 20:37:34 +0200

    it is worse now

--- source/Main.hx
+++ source/Main.hx
@@ -45,7 +45,7 @@ class Main extends Sprite
 
 		// Credits to MAJigsaw77 (he's the og author for this code)
 		#if android
-		//Sys.setCwd(Path.addTrailingSlash(SUtil.getPath()));
+		Sys.setCwd(Path.addTrailingSlash(SUtil.getPath()));
 		SUtil.gameCrashCheck();
 		#elseif ios
 		Sys.setCwd(lime.system.System.applicationStorageDirectory);
