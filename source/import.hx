7356a3f3315340e7009f75692b662a234e995b0e
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-14 10:39:40 +0200

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -19,20 +19,7 @@ import meta.data.Discord;
 
 import Init;
 #if mobile
-import mobile.utils.StorageUtil;
-#end 
-
-// Android
-#if android
-import android.content.Context as AndroidContext;
-import android.widget.Toast as AndroidToast;
-import android.os.Environment as AndroidEnvironment;
-import android.Permissions as AndroidPermissions;
-import android.Settings as AndroidSettings;
-import android.Tools as AndroidTools;
-import android.os.Build.VERSION as AndroidVersion;
-import android.os.Build.VERSION_CODES as AndroidVersionCode;
-import android.os.BatteryManager as AndroidBatteryManager;
+import mobile.utils.SUtil;
 #end
 
 #if sys
